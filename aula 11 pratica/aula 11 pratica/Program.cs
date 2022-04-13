e9e7a428109a1b41031de8087384e72c645cc1e9
MatheusRodriguesSilva <matheusmtt2323@gmail.com>
2022-04-13 01:43:54 -0300

    prática aula 10 e 11

---
+++ aula 11 pratica/aula 11 pratica/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PraticaAula11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lista!");
+
+            List<string> lista = new List<string>();
+
+            lista.Add("Matheus");
+            lista.Add("Murilo");
+            lista.Add("Lucas");
+            lista.Add("Guilherme");
+
+            foreach (string item in lista)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
